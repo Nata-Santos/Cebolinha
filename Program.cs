e10792bf41331543ef83463a2650edcea22a8d98
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-06 00:02:39 -0300

    Entrega da atividade Cebolinha

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string cebolinha;
+           Console.ForegroundColor = ConsoleColor.DarkCyan;
+           Console.WriteLine();
+           
+           Console.WriteLine("Digite uma frase, Ela será reproduzida com a fala do Cebolinha");
+           Console.WriteLine();
+           Console.ForegroundColor = ConsoleColor.DarkGreen;
+           cebolinha = Console.ReadLine();
+           Console.WriteLine();
+           Console.WriteLine($"{cebolinha.Replace("r", "l")}");
+           Console.ResetColor();    
+        }
+    }
+}
